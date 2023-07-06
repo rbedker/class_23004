8e5bad02e9e051f617fb015e04b1990550b39cd5
Lramos808 <138782816+Lramos808@users.noreply.github.com>
2023-07-06 07:25:26 -0500

    Update char_sheet.py

--- char_sheet.py
+++ char_sheet.py
@@ -2,7 +2,7 @@ line_list = []
 list_type = ["class_name", "atk",
              "dfns", "dod", "hp",
              "intel", "fth", "stgh", "dex"]
-dict_list = []
+class_list = []
 
 with open("char_stats.txt", 'r') as f:
     for line in f:
@@ -15,14 +15,14 @@ with open("char_stats.txt", 'r') as f:
 
 for characters in line_list:
     dict_char = dict(zip(list_type, characters))
-    char_dict.insert(-1, dict_char)
+    class_dict.insert(-1, dict_char)
 
 
-mage_stats = char_dict[0]
-cleric_stats = char_dict[1]
-monk_stats = char_dict[2]
-rogue_stats = char_dict[3]
-builder_stats = char_dict[4]
-barb_stats = char_dict[5]
-cowboy_stats = char_dict[6]
-paladin_stats = char_dict[7]
+mage_stats = class_dict[0]
+cleric_stats = class_dict[1]
+monk_stats = class_dict[2]
+rogue_stats = class_dict[3]
+builder_stats = class_dict[4]
+barb_stats = class_dict[5]
+cowboy_stats = class_dict[6]
+paladin_stats = class_dict[7]
